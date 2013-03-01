873ff2336e48c75294e3190666eebac162c21923
Ben Kreeger <ben@kree.gr>
2013-03-01 10:00:07 -0600

    Turning off incremental updates because they currently suck. Badly.

--- reindexer.rb
+++ reindexer.rb
@@ -4,16 +4,13 @@ class Reindexer
   def self.perform(data, force_rebuild)
     Gem.post_reset{ Gem::Specification.all = nil }
     indexer = Gem::Indexer.new(data)
-    if force_rebuild
+
+    begin
       indexer.generate_index
-    else
-      begin
-        indexer.update_index
-      rescue => e
-        puts "#{e.class}:#{e.message}"
-        puts e.backtrace.join("\n")
-        perform(indexer, true)
-      end
+    rescue => e
+      puts "#{e.class}:#{e.message}"
+      puts e.backtrace.join("\n")
+      perform(indexer, true)
     end
   end
 end
